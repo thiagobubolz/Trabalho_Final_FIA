9c05c3d604bb2c3c38bd9d2afec234522f18fa26
thiagobubolz <tlabubolz@inf.ufpel.edu.br>
2019-06-24 13:43:44 -0300

    Atualizando funções board

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+
 import board
 import numpy
+
+
+
+tabuleiro = board.Board()
+
+lista = tabuleiro.calculate_verticals()
+print("VERTICAIS")
+print(lista)
+print()
+
+lista = tabuleiro.calculate_main_diagonals()
+print("MAIN")
+print(lista)
+print()
+
+lista = tabuleiro.calculate_secondary_diagonals()
+print("SEC")
+print(lista)
+print()
 
