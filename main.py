6f340af0b08afebfa15810b239e8d222e9f38618
thiagobubolz <tlabubolz@inf.ufpel.edu.br>
2019-06-29 23:57:50 -0300

    iniciando minimax

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
+
+
+
+
+
+
+
+function minimax(position, depth, maximizingPlayer)
+	if depth == 0 or game over in position
+		return static evaluation of position
+
+	if maximizingPlayer
+		maxEval = -infinity
+		for each child of position
+			eval = minimax(child, depth - 1, false)
+			maxEval = max(maxEval, eval)
+		return maxEval
+
+	else
+		minEval = +infinity
+		for each child of position
+			eval = minimax(child, depth - 1, true)
+			minEval = min(minEval, eval)
+		return minEval
+
+
+// initial call
+minimax(currentPosition, 3, true)
+
 # -*- coding: utf-8 -*-
 
 import board
 import utils
 
-tabuleiro = board.Board()
+tabuleiro = board.Board(0)
 
 lista = tabuleiro.calculate_board_score()
 
--- minimax.py
+++ minimax.py
@@ -1,8 +1,45 @@
 # -*- coding: utf-8 -*-
 
 import board
+import utils
+import copy
+from math import inf
 
-def minimax(board, possible_moves, player):
-    scores = []
+def minimax(board, depth, maximizingPlayer, last_move):
+    possible_moves = utils.get_possible_moves(board)
+    score = board.calculate_board_score()
+
+    if depth == 0:
+        return 0
+
+    if maximizingPlayer:
+        maxEval = -inf
+        for move in possible_moves:
+            evalue, pos = minimax(utils.make_move(copy.deepcopy(board.get_board()), move, 1), depth-1, False)
+            if maxEval > evalue:
+
+
+        return
+
+    if score == 5000000:
+        return movimento, score
+
     for move in possible_moves:
-        
+        aux = utils.make_move(copy.deepcopy(board.get_board()), move, player)
+        aux.
+
+        if player == 1:
+            next_player = 2
+        else:
+            next_player = 1
+
+        minimax(aux, next_player)
+
+        scores.append(aux.calculate_board_score())
+        movimentos.append(move)
+
+    if player == 1:
+        score = max(scores)
+
+
+    return movimento, score
